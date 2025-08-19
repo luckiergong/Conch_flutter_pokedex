ad594b83dd955c94e25a3528d09a443ea3fa8766
luckiergong <luckiergong@tencent.com>
2025-08-19 13:52:40 +0800

    Conch3.16.9改造,统一测试流程

--- lib/main.dart
+++ lib/main.dart
@@ -9,42 +9,24 @@ import 'package:pokedex/data/source/local/local_datasource.dart';
 import 'package:pokedex/states/theme/theme_cubit.dart';
 import 'package:pokedex/states/item/item_bloc.dart';
 import 'package:pokedex/states/pokemon/pokemon_bloc.dart';
-import 'package:flutter_conch_plugin/annotation/patch_scope.dart';
 
-import 'package:flutter_conch_loader/flutter_conch_loader.dart';
+import 'package:flutter_conch_plugin/annotation/patch_scope.dart';
+import 'package:flutter_conch_plugin/conch_dispatch.dart';
+import 'package:flutter/services.dart';
+
+bool useConch = false;
 
 @PatchScope()
-void main() async {
+Future<void> main() async {
+  /// 确保初始化完成
   WidgetsFlutterBinding.ensureInitialized();
 
-  // 推荐使用shilpy平台配置
-  var fileLoader = ShiplyFileLoader(ResHubParams(
-      appVersion: "1.0.0",
-      deviceId: "12345",
-      appId: "6db6d7936d",
-      appKey: "a19b7300-e6f0-45f4-80d7-9a1986812f0b",
-      env: "online",
-      resId: "conch_test_pokedex",
-      awaitNetData: true));
-  // 初始化基础能力
-  // 初始化完成之后的操作
-  ConchLoader.getInstance().init(
-    showDebugTag: true,
-    customLoadPageReporter: (name, code, message) {
-      print("加载结果 name: $name, code: $code,message:$message");
-    },
-    onLoadingBuilder: () {
-      return Scaffold(
-        body: Center(
-          child: Text("初始化加载样式……"),
-        ),
-      );
-    },
-  );
-  // 绑定具体加载逻辑
-  ConchLoader.getInstance().bindFileLoader(fileLoader, isPreLoad: true);
-  ConchLoader.getInstance();
-
+  if (useConch) {
+    var source = await rootBundle.load('assets/conch_build/patch_dat/conch_result.dat');
+    ConchDispatch.instance.loadByteSource(source);
+    return await ConchDispatch.instance
+        .callStaticFun(library: 'package:pokedex/main.dart', funcName: 'mainInner');
+  }
   await mainInner();
 }
 
